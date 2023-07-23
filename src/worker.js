94e0651c53cf54ea359679b95193e5003fe16045
Razo <bestwishtous@gmail.com>
2023-07-23 17:06:21 +0800

    feature:  非ws请求携带一个 hostname 参数传递原始域名

--- src/worker.js
+++ src/worker.js
@@ -12,15 +12,22 @@ export default {
 	async fetch(request, env, ctx) {
 		const url = new URL(request.url);
 		const hostname = url.hostname;
-		const [,targetHost] = hostname.match(/(\S+)\.(proxy\.\S+)/) || []
+		const [, targetHost] = hostname.match(/(\S+)\.(proxy\.\S+)/) || [];
 		if (targetHost) {
 			if (url.pathname.startsWith('/')) {
 				url.hostname = targetHost;
+				const upgradeHeader = request.headers.get('Upgrade');
+				/**
+				 * 如果是非ws请求携带一个 hostname 参数传递原始域名 
+				 */
+				if (!upgradeHeader || upgradeHeader !== 'websocket') {
+					url.searchParams.set('hostname', hostname);
+				}
 				return fetch(new Request(url, request));
 			}
 			return env.ASSETS.fetch(request);
-		}else{
-			return new Response('Not found', { status: 404 })
+		} else {
+			return new Response('Not found', { status: 404 });
 		}
 	},
 };
