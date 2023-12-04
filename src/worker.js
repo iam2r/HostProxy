9e72ae0a407e5122e307fee0481dc97cc4286e8b
iamrazo <bestwishtous@gmail.com>
2023-12-04 13:21:39 +0800

    feature: 添加默认代理

--- src/worker.js
+++ src/worker.js
@@ -1,27 +1,19 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+const defaultTargetHost = '';
 export default {
 	async fetch(request, env) {
 		const url = new URL(request.url);
 		const hostname = url.hostname;
 		let [, targetHost] = hostname.match(/(\S+)\.(proxy\.\S+)/) || [];
-		if(!targetHost){
-			 targetHost = (hostname.match(/^proxy\.(\S+)/) || [])[1];
+		if (!targetHost) {
+			targetHost = (hostname.match(/^proxy\.(\S+)/) || [])[1];
 		}
+		targetHost = targetHost || defaultTargetHost;
 		if (targetHost) {
 			if (url.pathname.startsWith('/')) {
 				url.hostname = targetHost;
 				const upgradeHeader = request.headers.get('Upgrade');
 				/**
-				 * 如果是非ws请求携带一个 hostname 参数传递原始域名 
+				 * 如果是非ws请求携带一个 hostname 参数传递原始域名
 				 */
 				if (!upgradeHeader || upgradeHeader !== 'websocket') {
 					url.searchParams.set('hostname', hostname);
