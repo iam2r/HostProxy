96ecb819d30c6b017bb748416272560df3b6d3e7
iam2r <bestwishtous@gmail.com>
2023-12-06 04:31:04 +0000

    feature: update proxy methods

--- src/express.js
+++ src/express.js
@@ -5,28 +5,11 @@ const { getIp } = require('./common');
 const port = 8788;
 const app = express();
 app.use(cors());
-app.use('/', async (req, res) => {
-	const url = require('url').parse(req.url, true);
-	let [, targetHost] = url.pathname.split('/');
 
-	targetHost = targetHost || '';
-	if (targetHost) {
-		req.hostname = targetHost;
-		url.pathname = url.pathname.replace(new RegExp(`^/${targetHost}`), '');
-
-		req.url = require('url').format(url);
-		return createProxyMiddleware({
-			target: `https://${targetHost}`,
-			changeOrigin: true,
-			pathRewrite: {
-				[`^/${targetHost}`]: '',
-			},
-		})(req, res);
-	} else {
+const createServerInfoMiddleware = () => async (req, res, next) => {
+	try {
 		const serverIpData = await getIp();
-		// 设置响应的 Content-Type 为 application/json
 		res.setHeader('Content-Type', 'application/json');
-		// 发送格式化后的 JSON 响应
 		res.send(
 			JSON.stringify(
 				{
@@ -37,8 +20,33 @@ app.use('/', async (req, res) => {
 				2
 			)
 		);
+	} catch (error) {
+		next(error); // 将错误传递给 Express 的错误处理中间件
+	}
+};
+
+// 配置反向代理
+app.use('/:target?', async (req, res, next) => {
+	const { target } = req.params;
+	if (target) {
+		createProxyMiddleware({
+			target: `https://${target}`,
+			changeOrigin: true,
+			pathRewrite: {
+				[`^/${target}`]: '', // 将路径中的 `^/${target}` 替换为空字符串
+			},
+		})(req, res, next);
+	} else {
+		createServerInfoMiddleware()(req, res, next);
 	}
 });
+
+// 错误处理中间件
+app.use((err, req, res, next) => {
+	console.error(err);
+	res.status(500).send('Internal Server Error');
+});
+
 app.listen(port, () => {
 	console.log(`http://localhost:${port}`);
 });
--- src/worker.js
+++ src/worker.js
@@ -3,7 +3,7 @@ import { getIp } from './common';
 const defaultTargetHost = '';
 const corsHeaders = {
 	'Access-Control-Allow-Headers': '*', // What headers are allowed. * is wildcard. Instead of using '*', you can specify a list of specific headers that are allowed, such as: Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Authorization.
-	'Access-Control-Allow-Methods': 'POST', // Allowed methods. Others could be GET, PUT, DELETE etc.
+	'Access-Control-Allow-Methods': '*', // Allowed methods. Others could be GET, PUT, DELETE etc.
 	'Access-Control-Allow-Origin': '*', // This is URLs that are allowed to access the server. * is the wildcard character meaning any URL can.
 };
 async function handleOptions(request) {
